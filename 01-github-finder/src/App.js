686826f75a0062b3a16b442154d43abb8578a0c8
Timothy Ho <tho@cra.com>
2019-07-08 13:47:14 -0400

    refactored components into functional components, added axois for data fetching, added spinner, added github API client keys

--- 01-github-finder/src/App.js
+++ 01-github-finder/src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    // run this code right when the component loads
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <UserItem />
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
--- 01-github-finder/src/components/users/UserItem.js
+++ 01-github-finder/src/components/users/UserItem.js
@@ -1,36 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      id: 1,
-      login: 'mojombo',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo'
-    };
-  }
-
-  render() {
-    const { login, avatar_url, html_url } = this.state;
-
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt=''
-          className='round-img'
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
 
 export default UserItem;
