9295308115536276b1b88f2e7c7787bfdea94935
Timothy Ho <tho@cra.com>
2019-07-25 18:18:23 -0400

    finished testing JWT on successful login credentials

--- 02-contact-keeper/backend/routes/auth.js
+++ 02-contact-keeper/backend/routes/auth.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const User = require('../models/User');
 
 // use router for HTTP requests such as GET, POST, PUT (update), and DELETE
 
@@ -13,8 +19,51 @@ router.get('/', (req, res) => {
 // @route   POST api/auth
 // @desc    Authenticate user, get token
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Log in user');
-});
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Incorrect password' });
+      }
+
+      // create JWT
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send({ msg: 'Server Error' });
+    }
+  }
+);
 
 module.exports = router;
--- 02-contact-keeper/backend/server.js
+++ 02-contact-keeper/backend/server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
 connectDB();
+
+// Initialize Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to ContactKeeper API' }));
 
