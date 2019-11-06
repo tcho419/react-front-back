a69a8335b2c0da2abad7a5eec271e9da545550d7
Timothy Ho <tho@cra.com>
2019-11-06 18:06:18 -0500

    refactor and redo client side to reinforce react context concepts

--- 02-contact-keeper/client/src/components/layout/Navbar.jsx
+++ 02-contact-keeper/client/src/components/layout/Navbar.jsx
@@ -12,8 +12,6 @@ const Navbar = ({ title, icon }) => {
         <li>
           <Link to='/'>Home</Link>
           <Link to='/about'>About</Link>
-          <Link to='/register'>Register</Link>
-          <Link to='/login'>Login</Link>
         </li>
       </ul>
     </div>
