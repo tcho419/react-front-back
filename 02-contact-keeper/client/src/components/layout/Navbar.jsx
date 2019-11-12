97b3380c7e9b1b3ae2806d6d8994b7c3f61d06e8
Timothy Ho <tho@cra.com>
2019-11-12 17:53:30 -0500

    create basic setup for Auth context and components for Register and Login

--- 02-contact-keeper/client/src/components/layout/Navbar.jsx
+++ 02-contact-keeper/client/src/components/layout/Navbar.jsx
@@ -11,7 +11,15 @@ const Navbar = ({ title, icon }) => {
       <ul>
         <li>
           <Link to='/'>Home</Link>
+        </li>
+        <li>
           <Link to='/about'>About</Link>
+        </li>
+        <li>
+          <Link to='/register'>Register</Link>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
         </li>
       </ul>
     </div>
---
+++ 02-contact-keeper/client/src/context/auth/AuthState.jsx
@@ -0,0 +1,57 @@
+import React, { createContext, useReducer } from 'react';
+import authReducer from './authReducer';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS
+} from '../types';
+
+export const AuthContext = createContext();
+const { Provider } = AuthContext;
+
+const AuthState = ({ children }) => {
+  // set up initial state
+  const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    user: null,
+    error: null
+  };
+
+  // set up the useReducer hook
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  // set up the provider functions
+
+  // Load User
+
+  // Register User
+
+  // Login User
+
+  // Logout
+
+  // Clear Errors
+
+  return (
+    <Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error
+      }}
+    >
+      {children}
+    </Provider>
+  );
+};
+
+export default AuthState;
