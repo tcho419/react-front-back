a91d88a190a965bc439ce70b593063602c7b2e94
Timothy Ho <tho@cra.com>
2020-01-08 22:17:23 -0500

    set up all the UI and modals with basic console logs, will add redux next

--- 03-it-logger/src/App.js
+++ 03-it-logger/src/App.js
@@ -1,6 +1,11 @@
 import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddButton from './components/layout/AddButton';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -16,6 +21,11 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className="container">
+        <AddButton />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
---
+++ 03-it-logger/src/components/techs/TechListModal.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import TechItem from './TechItem';
+import axios from 'axios';
+
+const TechListModal = () => {
+  const [techs, setTechs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getTechs();
+    // eslint-disable-next-line
+  }, []);
+
+  const getTechs = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get('/techs');
+      setTechs(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div id="tech-list-modal" className="modal">
+      <div className="modal-content">
+        <h4>Technician List</h4>
+        <ul className="collection">
+          {!loading &&
+            techs.map(tech => <TechItem tech={tech} key={tech.id} />)}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TechListModal;
